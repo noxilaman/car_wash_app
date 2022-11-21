cde217d87b6d2824826206b650ab27261ef06f05
parinyalaco <parinya.k@lannaagro.com>
2022-11-21 16:53:18 +0700

    inital set and improve save

--- backend_nodejs/index.js
+++ backend_nodejs/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const connection = require('./config/databaseConnection');
+const multer = require("multer");
 
 const app = express();
 const port = 8086;
@@ -16,11 +17,99 @@ app.get('/api', (req, res) => {
     res.send('Hello World, from express');
 });
 
+app.get("/api/initial", (req, res) => {
+
+    var sizecarbase = [
+      "S",
+      "M",
+      "L",
+      "XL",
+    ];
+
+    var washtypebase = [
+      "ล้างสี",
+      "ดูดฝุ่น",
+      "ล้างสี + ดูดฝุ่น",
+      "ล้างสี + ดูดฝุ่น + ขัดเครือบ",
+    ];
+
+    var pricelists = [
+      ["ล้างสี", "S", "80"],
+      ["ล้างสี", "M", "80"],
+      ["ล้างสี", "L", "80"],
+      ["ล้างสี", "XL", "80"],
+      ["ดูดฝุ่น", "S", "80"],
+      ["ดูดฝุ่น", "M", "80"],
+      ["ดูดฝุ่น", "L", "80"],
+      ["ดูดฝุ่น", "XL", "80"],
+      ["ล้างสี + ดูดฝุ่น", "S", "80"],
+      ["ล้างสี + ดูดฝุ่น", "M", "80"],
+      ["ล้างสี + ดูดฝุ่น", "L", "80"],
+      ["ล้างสี + ดูดฝุ่น", "XL", "80"],
+      ["ล้างสี + ดูดฝุ่น + ขัดเครือบ", "S", "80"],
+      ["ล้างสี + ดูดฝุ่น + ขัดเครือบ", "M", "80"],
+      ["ล้างสี + ดูดฝุ่น + ขัดเครือบ", "L", "80"],
+      ["ล้างสี + ดูดฝุ่น + ขัดเครือบ", "XL", "80"],
+    ];
+
+
+    sizecarbase.map((opt) => {
+        try {
+            console.log(opt);
+            connection.query(
+              "INSERT INTO car_size (`name`, `desc`, created_at, modified_at) value (?,?,now(),now());",
+              [opt, opt],
+              function (error, results, fields) {
+                if (error) throw error;
+                console.log(results);
+              }
+            );
+        } catch (error) {
+          console.log(error);
+        }
+    });
+
+    washtypebase.map((opt) => {
+      try {
+        console.log(opt);
+        connection.query(
+          "INSERT INTO wash_types (`name`, `desc`, created_at, modified_at) value (?,?,now(),now());",
+          [opt, opt],
+          function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    pricelists.map((opt) => {
+      try {
+        // console.log(opt[0]);
+        connection.query(
+          "INSERT INTO prices (`wash_type_id`, `car_size_id`,price, created_at, modified_at) value ((select id from wash_types where `name` = ? limit 1),(select id from car_size where `name` = ? limit 1),?,now(),now());",
+          [opt[0], opt[1], opt[2]],
+          function (error, results, fields) {
+            if (error) throw error;
+            console.log(results);
+          }
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+
+});
+
+
 app.get('/api/sizecar/getall', (req, res) => {
     try {
         connection.query('select * from car_size;', function (error, results, fields) {
         if (error) throw error;
-        console.log('The solution is: ', results[0].solution);
+        console.log(results);
 
         return res.status(200).json(results);
         });
@@ -32,6 +121,44 @@ app.get('/api/sizecar/getall', (req, res) => {
     
 });
 
+app.get("/api/washtype/getall", (req, res) => {
+  try {
+    connection.query(
+      "select * from wash_types;",
+      function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+
+        return res.status(200).json(results);
+      }
+    );
+
+    //connection.end();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/api/price/getselected/:typewashid/:carsizeid", (req, res) => {
+  try {
+    connection.query(
+      "select price from prices where wash_type_id = ? and car_size_id = ? limit 1;",
+      [req.params.typewashid, req.params.carsizeid],
+      function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+
+        return res.status(200).json(results);
+      }
+    );
+
+    //connection.end();
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
 
 app.post('/api/washcar/create', (req, res) => {
     const postData = req.body;
@@ -40,11 +167,17 @@ app.post('/api/washcar/create', (req, res) => {
         [postData.licensename,postData.city,postData.sizeId], 
         function (error, results, fields) {
             if (error) throw error;
-             console.log('The solution is: ', results[0].solution);
-        });
-    
-   // connection.end();
-        
+             console.log(results);
+               connection.query(
+                 "INSERT INTO activities (car_id, wash_type_id, status, note, created_at, modified_at) values (?,?,'Pending','',now(),now())",
+                 [results.insertId, postData.washTypeId],
+                 function (error, results, fields) {
+                   if (error) throw error;
+                   console.log(results);
+                 }
+               );
+
+        }); 
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
--- frontend_reactjs/src/pages/Washpage.js
+++ frontend_reactjs/src/pages/Washpage.js
@@ -14,15 +14,29 @@ function Washpage() {
   const [sizeId,setSizeId] = useState('');
   const [washTypeId,setWashTypeId] = useState('');
   const [carSizes,setCarSize] = useState([]);
+  const [washTypes, setWashTypes] = useState([]);
   const [postResult,setPostResult] = useState(null);
+
+  // const washTypeId = useRef();
 
   useEffect(() => { 
     (async () => {
     try {
-      const res = await axios.get("http://localhost:8086/api/sizecar/getall").then(function (response) {
-        setCarSize(response.data);
-        console.log(response.data);
-      });
+      const res = await axios
+        .get("http://localhost:8086/api/sizecar/getall")
+        .then(function (response) {
+          setCarSize(response.data);
+          console.log(response.data);
+        });
+
+      const res2 = await axios
+        .get("http://localhost:8086/api/washtype/getall")
+        .then(function (response) {
+          setWashTypes(response.data);
+          console.log(response.data);
+        });
+
+        priceHandler();
 
     } catch (err) {
       setPostResult(fortmatResponse(err.response?.data || err));
@@ -44,11 +58,42 @@ function Washpage() {
   
   const sizeIdHandler = event => {
     setSizeId(event.target.value);
+     console.log(sizeId);
+  priceHandler(washTypeId,event.target.value);
   }
 
   const washTypeIdHandler = event => {
-    setWashTypeId(event.target.value);
+     setWashTypeId(event.target.value);
+    // console.log(washTypeId.current);
+    priceHandler(event.target.value, sizeId);
   }
+
+  const priceHandler = async (wtid,scid) => {
+    try {
+      console.log(
+        "http://localhost:8086/api/price/getselected/" + wtid + "/" + scid
+      );
+      if (
+        wtid !== "" &&
+        scid !== "" &&
+        wtid !== "Open this select menu" &&
+        scid !== "Open this select menu"
+      ) {
+        const res2 = await axios
+
+          .get(
+            "http://localhost:8086/api/price/getselected/" + wtid + "/" + scid
+          )
+          .then(function (response) {
+            // setWashTypes(response.data);
+            
+            console.log(response.data[0].price);
+          });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const postdata = async () => {
     const postData = {
@@ -93,11 +138,19 @@ function Washpage() {
             <Col>
               <Form.Group className="mb-3" controlId="formLicenseName">
                 <Form.Label>ทะเบียนรถ</Form.Label>
-                <Form.Control type="text" placeholder="ใส่ทะเบียนรถ" onChange={licenseNameHandler}/>
+                <Form.Control
+                  type="text"
+                  placeholder="ใส่ทะเบียนรถ"
+                  onChange={licenseNameHandler}
+                />
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formLicenseCity" >
+              <Form.Group className="mb-3" controlId="formLicenseCity">
                 <Form.Label>จังหวัด</Form.Label>
-                <Form.Control type="text" placeholder="ใส่ทะเบียนจังหวัด" onChange={cityHandler}/>
+                <Form.Control
+                  type="text"
+                  placeholder="ใส่ทะเบียนจังหวัด"
+                  onChange={cityHandler}
+                />
               </Form.Group>
               <Form.Group controlId="formFile" className="mb-3">
                 <Form.Label>รูปรถ</Form.Label>
@@ -107,26 +160,31 @@ function Washpage() {
             <Col>
               <Form.Group className="mb-3" controlId="formLicenseName">
                 <Form.Label>ขนาดรถ</Form.Label>
-                <Form.Select aria-label="Default select example" onChange={sizeIdHandler}>
-                <option>Open this select menu</option>
-                {carSizes.map(opt => (
-                  <option value={opt.id}>{opt.name}</option>
-                ))}
+                <Form.Select
+                  aria-label="Default select example"
+                  value={sizeId}
+                  onChange={sizeIdHandler}
+                >
+                  <option>Open this select menu</option>
+                  {carSizes.map((opt) => (
+                    <option value={opt.id}>{opt.name}</option>
+                  ))}
                 </Form.Select>
               </Form.Group>
               <Form.Group className="mb-3" controlId="formLicenseCity">
                 <Form.Label>บริการ</Form.Label>
-                <Form.Select aria-label="Default select example" onChange={washTypeIdHandler}>
+                <Form.Select
+                  aria-label="Default select example"
+                  value={washTypeId}
+                  onChange={washTypeIdHandler}
+                >
                   <option>Open this select menu</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
+                  {washTypes.map((opt) => (
+                    <option value={opt.id}>{opt.name}</option>
+                  ))}
                 </Form.Select>
               </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
-              <Button variant="primary" onClick={postdata} >
+              <Button variant="primary" onClick={postdata}>
                 Submit
               </Button>
             </Col>
