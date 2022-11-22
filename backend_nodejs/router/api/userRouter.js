d7eb2eb135a1bd9aa3a3f9ada3ee97a0025fdb1f
Pong CPE <pong.cpe@gmail.com>
2022-11-23 00:33:34 +0700

    jwt and bcrypt

---
+++ backend_nodejs/router/api/userRouter.js
@@ -0,0 +1,7 @@
+var express = require("express");
+const connection = require("../../config/databaseConnection");
+var router = express.Router();
+
+//https://www.youtube.com/watch?v=XL5SBUZU35s
+
+module.exports = router;
