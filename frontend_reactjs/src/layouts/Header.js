287b99bf448a8152cf0b7eee4f0bdfd963cf13af
parinyalaco <parinya.k@lannaagro.com>
2022-12-12 11:09:45 +0700

    window code

--- frontend_reactjs/src/layouts/Header.js
+++ frontend_reactjs/src/layouts/Header.js
@@ -10,9 +10,13 @@ function Header() {
   const navigate = useNavigate()
 
   const tokenkey = localStorage.getItem("token");
+  const shop_id = localStorage.getItem("shop_id");
+
+  console.log(tokenkey);
 
   const logoutHandler = ()=> {
     localStorage.setItem("token","");
+    localStorage.setItem("shop_id", "");
     navigate("/login");
   }
 
@@ -22,34 +26,43 @@ function Header() {
         <Navbar.Brand href="/">Car Wash App</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          {tokenkey.length > 0 && (
+          {tokenkey !== null &&
+            tokenkey.length > 0 &&
+            shop_id !== null &&
+            shop_id.length > 0 && (
+              <Nav className="me-auto">
+                <Nav.Link href="/dashboardpage">Dashboard</Nav.Link>
+                <Nav.Link href="/washpage">ล้างรถ</Nav.Link>
+                <Nav.Link href="/listpage">รายการ</Nav.Link>
+                <NavDropdown title="Base Data" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/admin/user/list">
+                    พนักงาน
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/admin/carsize/list">
+                    ขนาดรถ
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/admin/washtype/list">
+                    บริการ
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/admin/price/list">
+                    ราคาบริการ
+                  </NavDropdown.Item>
+                </NavDropdown>
+                <Nav.Link href="#" onClick={logoutHandler}>
+                  Logout
+                </Nav.Link>
+              </Nav>
+            )}
+          {(tokenkey === null || tokenkey.length === 0) && (
             <Nav className="me-auto">
-              <Nav.Link href="/dashboardpage">Dashboard</Nav.Link>
-              <Nav.Link href="/washpage">ล้างรถ</Nav.Link>
-              <Nav.Link href="/listpage">รายการ</Nav.Link>
-              <NavDropdown title="Base Data" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/admin/user/list">
-                  พนักงาน
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/admin/carsize/list">
-                  ขนาดรถ
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/admin/washtype/list">
-                  บริการ
-                </NavDropdown.Item>
-                <NavDropdown.Item href="/admin/price/list">
-                  ราคาบริการ
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="#" onClick={logoutHandler}>
-                Logout
-              </Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
             </Nav>
           )}
-          {tokenkey.length == 0 && (
+          {(shop_id === null || shop_id.length === 0) && (
             <Nav className="me-auto">
-              <Nav.Link href="/login" >
-                Login
+              <Nav.Link href="/">เลือกร้าน</Nav.Link>
+              <Nav.Link href="#" onClick={logoutHandler}>
+                Logout
               </Nav.Link>
             </Nav>
           )}
